1d70819751c420b834c702c32fc6384ecba263f8
SWood Teach <91884551+SWood-Comp@users.noreply.github.com>
2022-06-22 12:09:17 +0100

    Add files via upload

---
+++ CipherEncoder/CipherEncoder/CodeCipher.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace CipherEncoder
+{
+    partial class CodeCipher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPlaintext = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCiphertext = new System.Windows.Forms.TextBox();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnSuper = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPlaintext
+            // 
+            this.txtPlaintext.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPlaintext.Location = new System.Drawing.Point(18, 104);
+            this.txtPlaintext.Multiline = true;
+            this.txtPlaintext.Name = "txtPlaintext";
+            this.txtPlaintext.Size = new System.Drawing.Size(282, 165);
+            this.txtPlaintext.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Quartz MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.OrangeRed;
+            this.lblTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(580, 33);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Cipher - Text Encryption System v1.0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Plaintext message:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(430, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "CipherText message:";
+            // 
+            // txtCiphertext
+            // 
+            this.txtCiphertext.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCiphertext.Location = new System.Drawing.Point(434, 104);
+            this.txtCiphertext.Multiline = true;
+            this.txtCiphertext.Name = "txtCiphertext";
+            this.txtCiphertext.Size = new System.Drawing.Size(282, 165);
+            this.txtCiphertext.TabIndex = 5;
+            // 
+            // txtKey
+            // 
+            this.txtKey.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKey.Location = new System.Drawing.Point(151, 284);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(100, 29);
+            this.txtKey.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(14, 288);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Encryption Key:";
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnEncrypt.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncrypt.Location = new System.Drawing.Point(306, 104);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(122, 51);
+            this.btnEncrypt.TabIndex = 2;
+            this.btnEncrypt.Text = "Encrypt Message";
+            this.btnEncrypt.UseVisualStyleBackColor = false;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.BackColor = System.Drawing.Color.Orange;
+            this.btnDecrypt.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDecrypt.Location = new System.Drawing.Point(306, 218);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(122, 51);
+            this.btnDecrypt.TabIndex = 4;
+            this.btnDecrypt.Text = "Decrypt Message";
+            this.btnDecrypt.UseVisualStyleBackColor = false;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnSuper
+            // 
+            this.btnSuper.BackColor = System.Drawing.Color.LightBlue;
+            this.btnSuper.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSuper.Location = new System.Drawing.Point(306, 161);
+            this.btnSuper.Name = "btnSuper";
+            this.btnSuper.Size = new System.Drawing.Size(122, 51);
+            this.btnSuper.TabIndex = 3;
+            this.btnSuper.Text = "Super Encryption";
+            this.btnSuper.UseVisualStyleBackColor = false;
+            this.btnSuper.Click += new System.EventHandler(this.btnSuper_Click);
+            // 
+            // CodeCipher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(728, 337);
+            this.Controls.Add(this.btnSuper);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCiphertext);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txtPlaintext);
+            this.Name = "CodeCipher";
+            this.Text = "Cipher v1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPlaintext;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCiphertext;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnSuper;
+    }
+}
+
---
+++ CipherEncoder/CipherEncoder/CodeCipher.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CipherEncoder
+{
+    public partial class CodeCipher : Form
+    {
+        public CodeCipher()
+        {
+            InitializeComponent();
+            txtPlaintext.Focus();
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            int offsetKey;
+            offsetKey = random_Generator_Key();
+            encryptMessage(offsetKey);
+        }
+
+        private int random_Generator_Key()
+        {
+            int offset_Factor = 0;          // This generates a random number between 33 and 126 as specified.
+            string encryptionKey = "";      // Set up an empty encryptionKey, ready to store the random 8 characters.
+            Random rnd = new Random();
+
+            for (int x = 0; x < 8; x++)
+            {
+                
+                int randomKey = rnd.Next(33, 126);
+                char currentKey = (char)randomKey;
+                encryptionKey = encryptionKey + currentKey;
+            }
+
+            txtKey.Text = encryptionKey;
+
+            int keyTotal = 0;    // Sets up running total as 0.
+
+            foreach (char convertChar in encryptionKey)           // Sets current Character looked at as 0.
+            {
+                // Calculate the new offset factor to use...
+                int numASCII = (int)convertChar;
+                // keeps a running total here to perform the maths on later.
+                keyTotal += numASCII;
+            }
+
+            // divide the total by 8 and round it down. I used math.floor() to do this.
+            decimal offset1 = (keyTotal / 8);
+            offset1 = Math.Floor(offset1);
+            offset_Factor = ((int)offset1 - 32);
+
+            return offset_Factor;
+        }
+
+        private void encryptMessage(int offset_Factor)
+        {
+            string encryptedText = "";      // Set up 'encrypted' variable to hold all of the encrypted message.
+            string plainText = txtPlaintext.Text;
+            int maxChar = plainText.Length;
+            char encryptChar;
+
+
+            foreach (char currentChar in plainText)
+            {
+                int numASCII = (int)currentChar;
+
+                //if the ASCII value is 32, it is a [space] so keep it the same.
+                if (numASCII == 32)
+                    encryptChar = (char)numASCII;
+                else
+                {
+                    // add the ASCII number to the offset factor.
+                    int newNum = numASCII + offset_Factor;
+                    //if the number is greater than 126, subtract 94 to make it a valid ASCII number.
+                    if (newNum > 126)
+                    {
+                        newNum = newNum - 94;
+                        encryptChar = (char)newNum;
+                    }
+                    else
+                    {
+                        encryptChar = (char)newNum;
+                    }
+                }
+
+                // insert the newly encrypted character into the 'Encrypted' string.
+                encryptedText = encryptedText + encryptChar;
+
+                // increase the index of your 'slice' for the file being encrypted.
+                // this moves it on to the next character to be encrypted.
+
+                txtCiphertext.Text = encryptedText;
+            }
+        }
+
+        
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            int offsetKey;
+            offsetKey = decrypt_Key_Generator();
+            decryptMessage(offsetKey);
+        }
+
+        private int decrypt_Key_Generator()
+        {
+            int decrypt_Factor;         // Sets current Character as 0.
+            int decryptKeyTotal = 0;
+
+            string decryptionKey = txtKey.Text;
+
+            foreach (char convertChar in decryptionKey)
+            {
+                // Calculate the new offset factor to use...
+                int numASCII = (int)convertChar;
+                // keeps a running total here to perform the maths on later.
+                decryptKeyTotal += numASCII;
+            }
+
+            double offset1 = decryptKeyTotal / 8;
+            offset1 = Math.Floor(offset1);
+            decrypt_Factor = ((int)offset1 - 32);
+
+            return decrypt_Factor;
+        }
+
+        private void decryptMessage(int offset_Factor)
+        {
+            string decryptedText = "";      // Set up 'encrypted' variable to hold all of the encrypted message.
+            string cipherText = txtCiphertext.Text;
+            int maxChar = cipherText.Length;
+            char decryptChar;
+
+
+            foreach (char currentChar in cipherText)
+            {
+                int numASCII = (int)currentChar;
+
+                //if the ASCII value is 32, it is a [space] so keep it the same.
+                if (numASCII == 32)
+                    decryptChar = (char)numASCII;
+                else
+                {
+                    // add the ASCII number to the offset factor.
+                    int newNum = numASCII - offset_Factor;
+                    //if the number is greater than 126, subtract 94 to make it a valid ASCII number.
+                    if (newNum > 126)
+                    {
+                        newNum = newNum - 94;
+                        decryptChar = (char)newNum;
+                    }
+                    if (newNum < 33)
+                    {
+                        newNum = newNum + 94;
+                        decryptChar = (char)newNum;
+                    }
+                    else
+                    {
+                        decryptChar = (char)newNum;
+                    }
+                }
+
+                // insert the newly encrypted character into the 'Encrypted' string.
+                decryptedText = decryptedText + decryptChar;
+
+                // increase the index of your 'slice' for the file being encrypted.
+                // this moves it on to the next character to be encrypted.
+
+                txtPlaintext.Text = decryptedText;
+            }
+        }
+
+        private void btnSuper_Click(object sender, EventArgs e)
+        {
+            //int offsetKey;
+            //offsetKey = random_Generator_Key();
+            specialEncryption();
+        }
+
+        private void specialEncryption()
+        {
+            int blockCounter = 0;       // Sets up a blockCounter
+            string encryptedText = "";
+
+            string originalText = txtPlaintext.Text;
+
+            foreach (char testChar in originalText)               // For each character in your message.
+            {
+                if (((int)testChar) == 32)                        // Tests to see if current character is a space.
+                {
+                    encryptedText = encryptedText + "";           // If a space, then enter nothing into the message.
+                }
+                else
+                {
+                    encryptedText = encryptedText + testChar;     // If not a space then add in the character to new message   
+                    blockCounter += 1;                            // Add 1 to blockCounter variable.
+                }
+                if (blockCounter == 5)                            // When blockCounter hits 5.
+                {
+                    encryptedText = encryptedText + " ";          // Then it puts a space in and starts a new block.
+                    blockCounter = 0;                             // Resets blockCounter for next block of 5.
+                }
+            }
+
+            txtCiphertext.Text = encryptedText;
+
+        }
+    }
+}
